ef7c5bf6d9490b3e9d4c7bc54fdd74c082a91be6
lesham <leshadavay@gmail.com>
2022-06-06 08:27:57 +0500

    main page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
 import React from "react";
 
-const Home = () => {
+import { client } from "../lib/client";
+import { FooterBanner, HeroBanner, Product } from "../components";
+
+const Home = ({ products, banners }) => {
   return (
     <>
-      Hero Banner
-      <div>
+      <HeroBanner banner={banners.length && banners[0]} />
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
-        <div>{["Product1", "Product2"].map((product) => product)}</div>
       </div>
-      Footer
+      <div className="products-container">
+        {products.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={banners.length && banners[0]} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type=="product"]';
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type=="banner"]';
+  const banners = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, banners },
+  };
 };
 
 export default Home;
---
+++ pages/product/[slug].js
@@ -0,0 +1,79 @@
+import React from "react";
+import { client, urlFor } from "../../lib/client";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
+
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image && image[0])} />
+          </div>
+          <div className="small-images-container">
+            {image?.map((item, index) => (
+              <img src={urlFor(item)} className="" onMouseEnter="" />
+            ))}
+          </div>
+        </div>
+        <div className="product-details-desc">
+          <h1>{name}</h1>
+          <div className="reveiws">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type=="product"] {
+    slug {
+      current
+    }
+  }`;
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+  console.log("path: ", paths);
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const productQuery = `*[_type=="product" && slug.current == '${slug}'][0]`;
+  const productsQuery = `*[_type=="product"]`;
+
+  const product = await client.fetch(productQuery);
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { product, products },
+  };
+};
+
+export default ProductDetails;
